653c9d75d5177fdd4ac5a5be048e817d14ab7999
joealmond <jozsef.mandula@gmail.com>
2023-07-24 19:24:48 +0200

    done kitten list

--- client/src/Pages/EmployeeKittens.jsx
+++ client/src/Pages/EmployeeKittens.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const EmployeeKittens = () => {
-    const [employee, setEmployee ] = useState();
-    const { id } = useParams();
+  const [employee, setEmployee] = useState();
+  const { id } = useParams();
 
-    useEffect(()=>{
-
-        const fetchEmployee = async () => {
-            const res = await fetch(`/api/employees/${id}`);
-            const data = await res.json();
-            setEmployee(data);
-        };
-        fetchEmployee();
-
-    },[id])
-
+  useEffect(() => {
+    const fetchEmployee = async () => {
+      const res = await fetch(`/api/employees/${id}`);
+      const data = await res.json();
+      setEmployee(data);
+    };
+    fetchEmployee();
+  }, [id]);
   return (
-    <ul>{employee?.kittens.map((kitten)=><li><span>{kitten.name} </span> - <span> {kitten.weight} kg.</span></li>)}</ul>
-  )
-}
+    <ul>
+      {employee?.kittens.map((kitten) => (
+        <li key={kitten._id}>
+          <span>{kitten.name} </span> - <span> {kitten.weight} kg.</span>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default EmployeeKittens
+export default EmployeeKittens;
