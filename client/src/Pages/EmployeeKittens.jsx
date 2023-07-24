e8de3af81217d3ad5b71f158fdca045113998085
joealmond <jozsef.mandula@gmail.com>
2023-07-24 22:46:14 +0200

    done #6

--- client/src/Pages/EmployeeKittens.jsx
+++ client/src/Pages/EmployeeKittens.jsx
@@ -2,8 +2,41 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const EmployeeKittens = () => {
-  const [employee, setEmployee] = useState();
+  const [employee, setEmployee] = useState([]);
+  const [kittenName, setKittenName] = useState("");
+  const [kittenWeight, setKittenWeight] = useState("");
   const { id } = useParams();
+
+  const handleAddKitten = (e, id) => {
+    e.preventDefault();
+    try {
+      const addKitten = async () => {
+        const res = await fetch(`/api/employees/${id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...employee,
+            kittens: [
+              ...employee.kittens,
+              { name: kittenName, weight: kittenWeight },
+            ],
+          }),
+        });
+  
+        if (!res.ok) throw new Error('Could not add item!');
+  
+        const data = await res.json();
+        setEmployee(data)
+        setKittenName("")
+        setKittenWeight("")
+      };
+      addKitten();
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
   useEffect(() => {
     const fetchEmployee = async () => {
@@ -13,14 +46,40 @@ const EmployeeKittens = () => {
     };
     fetchEmployee();
   }, [id]);
+
   return (
-    <ul>
-      {employee?.kittens.map((kitten) => (
-        <li key={kitten._id}>
-          <span>{kitten.name} </span> - <span> {kitten.weight} kg.</span>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <form onSubmit={(e) => handleAddKitten(e, employee._id)}>
+        <h3>Add Kitten for {employee?.name}:</h3>
+        <label>
+          Kitten name:{" "}
+          <input
+            type="text"
+            value={kittenName}
+            onChange={(e) => setKittenName(e.target.value)}
+          />
+        </label>
+        <label>
+          Kitten weight:{" "}
+          <input
+            type="number"
+            value={kittenWeight}
+            onChange={(e) => setKittenWeight(e.target.value)}
+          />
+        </label>
+        <label>
+          <input type="submit" value="Add kitten" />
+        </label>
+      </form>
+      <h3>Kittens owned by {employee?.name}:</h3>
+      <ul>
+        {employee?.kittens?.map((kitten) => (
+          <li key={kitten._id}>
+            <span>{kitten.name} </span> - <span> {kitten.weight} kg.</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
