11801677c249f049b53ff3d84bbb102673f047c9
joealmond <jozsef.mandula@gmail.com>
2023-07-24 15:01:09 +0200

    listed employee kitten on kittens/:emp_ID

---
+++ client/src/Pages/EmployeeKittens.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+const EmployeeKittens = () => {
+    const [employee, setEmployee ] = useState();
+    const { id } = useParams();
+
+    useEffect(()=>{
+
+        const fetchEmployee = async () => {
+            const res = await fetch(`/api/employees/${id}`);
+            const data = await res.json();
+            setEmployee(data);
+        };
+        fetchEmployee();
+
+    },[id])
+
+  return (
+    <ul>{employee?.kittens.map((kitten)=><li><span>{kitten.name} </span> - <span> {kitten.weight} kg.</span></li>)}</ul>
+  )
+}
+
+export default EmployeeKittens
