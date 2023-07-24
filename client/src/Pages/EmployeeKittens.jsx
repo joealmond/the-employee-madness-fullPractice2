ffe2a83450542bc3fc7ae19f12ae0130b652c441
joealmond <jozsef.mandula@gmail.com>
2023-07-24 15:33:59 +0200

    implemented controlled input on kittens

--- client/src/Pages/EmployeeKittens.jsx
+++ client/src/Pages/EmployeeKittens.jsx
@@ -1,24 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const EmployeeKittens = () => {
-    const [employee, setEmployee ] = useState();
-    const { id } = useParams();
+  const [employee, setEmployee] = useState([]);
+  const [kittenName, setKittenName] = useState(null);
+  const [kittenWeight, setKittenWeight] = useState(null);
+  const { id } = useParams();
 
-    useEffect(()=>{
-
-        const fetchEmployee = async () => {
-            const res = await fetch(`/api/employees/${id}`);
-            const data = await res.json();
-            setEmployee(data);
-        };
-        fetchEmployee();
-
-    },[id])
+  useEffect(() => {
+    const fetchEmployee = async () => {
+      const res = await fetch(`/api/employees/${id}`);
+      const data = await res.json();
+      setEmployee(data);
+    };
+    fetchEmployee();
+  }, [id]);
 
   return (
-    <ul>{employee?.kittens.map((kitten)=><li><span>{kitten.name} </span> - <span> {kitten.weight} kg.</span></li>)}</ul>
-  )
-}
+    <ul>
+      {employee?.kittens?.map((kitten) => (
+        <li key={kitten._id}>
+          <input
+            type="text"
+            value={kittenName ?? kitten.name}
+            onChange={(e) => setKittenName(e.target.value)}
+          />
+          {" - "}
+          <input
+            type="text"
+            value={kittenWeight ?? kitten.weight}
+            onChange={(e) => setKittenWeight(e.target.value)}
+          />{" "}
+          kg.
+        </li>
+      ))}
+    </ul>
+  );
+};
 
-export default EmployeeKittens
+export default EmployeeKittens;
